66e6cdc48f7d5618f7b25b73af142fa0c8cd1d4f
Jernej Vivod <je.vivod@gmail.com>
2019-03-29 11:39:21 +0100

    Fix stopwatch layout

--- app/src/main/java/si/uni_lj/fri/pbd2019/runsup/MapsActivity.java
+++ app/src/main/java/si/uni_lj/fri/pbd2019/runsup/MapsActivity.java
@@ -28,7 +28,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker near Pyongyang, North Korea.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(39.0392, 125.7625);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Pyongyang"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
