8cbf28766c24f97e7265ec4817ee61fc5d7db246
Jernej Vivod <je.vivod@gmail.com>
2019-05-06 15:46:30 +0200

    Add activity and make layout

---
+++ app/src/main/java/si/uni_lj/fri/pbd2019/runsup/ActiveWorkoutMapActivity.java
@@ -0,0 +1,13 @@
+package si.uni_lj.fri.pbd2019.runsup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActiveWorkoutMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_active_workout_map);
+    }
+}
